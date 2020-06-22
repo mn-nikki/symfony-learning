aebe4ab616d447c25d152c9b1307715dbe01ad2d
Andrew Zhdanovskih <azh@crtweb.ru>
2020-06-22 14:34:28 +0500

    Add DI brief

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -1,13 +1,8 @@
 <?php declare(strict_types=1);
-/**
- * 17.06.2020
- */
-
 
 namespace App\Controller;
 
-
-use App\Repository\PizzaRepository;
+use App\Service\PizzaManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,26 +10,27 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class PizzaController extends AbstractController
 {
-    private PizzaRepository $pizzaRepository;
     private TranslatorInterface $translator;
+    private PizzaManagerInterface $manager;
 
-    public function __construct(PizzaRepository $pizzaRepository, TranslatorInterface $translator)
+    public function __construct(PizzaManagerInterface $manager, TranslatorInterface $translator)
     {
-        $this->pizzaRepository = $pizzaRepository;
         $this->translator = $translator;
+        $this->manager = $manager;
     }
 
     /**
      * @Route(path="/pizza/{page<\d+>?1}")
      *
      * @param int|null $page
+     *
      * @return Response
      */
     public function index(?int $page = null): Response
     {
         return $this->render('pizza/index.html.twig', [
-            'data' => $this->pizzaRepository->findAll(),
-            'title' => $this->translator->trans('pizza.title')
+            'data' => $this->manager->getRepository()->findAll(),
+            'title' => $this->translator->trans('pizza.title'),
         ]);
     }
 
@@ -42,12 +38,13 @@ class PizzaController extends AbstractController
      * @Route(path="/pizza/parts/{count<\d+>}")
      *
      * @param int $count
+     *
      * @return Response
      */
     public function withIngredients(int $count): Response
     {
         return $this->render('pizza/index.html.twig', [
-            'data' => $this->pizzaRepository->withNIngredients($count),
+            'data' => $this->manager->getRepository()->withNIngredients($count),
         ]);
     }
 }
